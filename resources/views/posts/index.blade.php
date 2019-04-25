55758fa6e6808a59ff7db73e0e20f7387a55ddbc
juarisco <juarisco@outlook.com>
2019-04-25 16:30:35 -0500

    12. Displaying all posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,16 +9,25 @@
         <div class="card-body">
             <table class="table">
                 <thead>
-                    <th>Name</th>
-                    <th></th>
+                    <th>Image</th>
+                    <th>Title</th>
+                    <th>Title</th>
+                    <th>Title</th>
                 </thead>
                 <tbody>
                     @foreach ($posts as $post)
                         <tr>
-                            <td>{{ $post->name }}</td>
+                            <td>
+                                {{-- <img src="{{ asset('storage/' . $post->image) }}" width="120px" height="60px" alt=""> --}}
+                                <img src="{{ Storage::url($post->image) }}" width="120px" height="60px" alt="">
+                            </td>
+                            <td>{{ $post->title }}</td>
                             <td>
                                 <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-info btn-sm">Edit</a>
-                                <button class="btn btn-danger btn-sm" onclick="handleDelete({{ $post->id }})">Delete</button>
+                            </td>
+                            <td>
+                                <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-danger btn-sm">Trash</a>
+                                {{-- <button class="btn btn-danger btn-sm" onclick="handleDelete({{ $post->id }})">Delete</button> --}}
                             </td>
                         </tr>
                     @endforeach
@@ -26,7 +35,7 @@
             </table>
 
             <!-- Modal -->
-            <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+            {{-- <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <form action="" method="POST" id="deletepostForm">
                         @csrf
@@ -50,19 +59,19 @@
                         </div>                        
                     </form>
                 </div>
-            </div>
+            </div> --}}
 
         </div>
     </div>
 @endsection
 
 @section('scripts')
-    <script>
+    {{-- <script>
         function handleDelete(id) {
             var form = document.getElementById('deletepostForm');
             form.action ='/posts/' + id
             console.log('deleting.', form)
             $('#deleteModal').modal('show')
         }
-    </script>
+    </script> --}}
 @endsection
