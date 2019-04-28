75486982362ea8afb5e8fbabd1ed5da8624d1801
juarisco <juarisco@outlook.com>
2019-04-28 16:20:50 -0500

    15. Permanently deleting models

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -94,12 +94,30 @@ class PostsController extends Controller
      * @param  \App\Post  $post
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Post $post)
+    public function destroy($id)
     {
-        $post->delete();
+        $post = Post::withTrashed()->where('id', $id)->firstOrFail();
 
-        session()->flash('success', 'Post trashed successfully.');
+        if ($post->trashed()) {
+            $post->forceDelete();
+        } else {
+            $post->delete();
+        }
+
+        session()->flash('success', 'Post deleted successfully.');
 
         return redirect(route('posts.index'));
+    }
+
+    /**
+     * Display a list of all trashed posts.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function trashed()
+    {
+        $trashed = Post::withTrashed()->get();
+
+        return view('posts.index')->withPosts($trashed);
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,75 +7,46 @@
     <div class="card card-default">
         <div class="card-header">Posts</div>
         <div class="card-body">
-            <table class="table">
-                <thead>
-                    <th>Image</th>
-                    <th>Title</th>
-                    <th>Title</th>
-                    <th>Title</th>
-                </thead>
-                <tbody>
-                    @foreach ($posts as $post)
-                        <tr>
-                            <td>
-                                {{-- <img src="{{ asset('storage/' . $post->image) }}" width="120px" height="60px" alt=""> --}}
-                                <img src="{{ Storage::url($post->image) }}" width="120px" height="60px" alt="">
-                            </td>
-                            <td>{{ $post->title }}</td>
-                            <td>
-                                <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-info btn-sm">Edit</a>
-                            </td>
-                            <td>
-                                <form action="{{ route('posts.destroy',$post->id) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger btn-sm">Trash</button>
-                                </form>
-                                {{-- <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-danger btn-sm">Trash</a> --}}
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            @if ($posts->count() > 0)
+                <table class="table">
+                    <thead>
+                        <th>Image</th>
+                        <th>Title</th>
+                        <th>Title</th>
+                        <th>Title</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($posts as $post)
+                            <tr>
+                                <td>
+                                    {{-- <img src="{{ asset('storage/' . $post->image) }}" width="120px" height="60px" alt=""> --}}
+                                    <img src="{{ Storage::url($post->image) }}" width="120px" height="60px" alt="">
+                                </td>
+                                <td>{{ $post->title }}</td>
 
-            <!-- Modal -->
-            {{-- <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <form action="" method="POST" id="deletepostForm">
-                        @csrf
-                        @method('DELETE')
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="deleteModalLabel">Delete post</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <p class="text-center text-bold">
-                                    Are you sure want to delete this post ? 
-                                </p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">No, Go back</button>
-                                <button type="submit" class="btn btn-danger">Yes, Delete</button>
-                            </div>
-                        </div>                        
-                    </form>
-                </div>
-            </div> --}}
+                                @if (!$post->trashed())
+                                    <td>
+                                        <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-info btn-sm">Edit</a>
+                                    </td>
+                                @endif
 
+                                <td>
+                                    <form action="{{ route('posts.destroy',$post->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger btn-sm">
+                                            {{ $post->trashed() ? 'Delete' : 'Trash' }}
+                                        </button>
+                                    </form>
+                                    {{-- <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-danger btn-sm">Trash</a> --}}
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            @else
+                <h3 class="text-center">No Posts Yet</h3>
+            @endif
         </div>
     </div>
-@endsection
-
-@section('scripts')
-    {{-- <script>
-        function handleDelete(id) {
-            var form = document.getElementById('deletepostForm');
-            form.action ='/posts/' + id
-            console.log('deleting.', form)
-            $('#deleteModal').modal('show')
-        }
-    </script> --}}
 @endsection
